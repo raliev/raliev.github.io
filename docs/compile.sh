fea575799ac4bd43a4506d3345cbda67ac82b648
Rauf Aliev <rauf.aliev@contracted.smpmi.com>
2025-09-04 15:00:58 -0400

    iiiid

--- docs/compile.sh
+++ docs/compile.sh
@@ -1,3 +1,3 @@
 rm -rf ../blog
 git rm -r --cached ../blog
-jekyll build && mv blog ../ && mkdir ../blog/pdfs && git add ../blog/ && find ../tex -name "*.pdf" | while read p; do cp $p ../blog/pdfs/; done && echo "SUCCESS!"
+jekyll build && mv blog ../ &&  git add ../blog/ && echo "SUCCESS!"
